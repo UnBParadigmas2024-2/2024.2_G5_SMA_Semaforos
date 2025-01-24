76f5d374567221b6649e8ba7957c738bba4b82ae
vnsrz <fowardshift@gmail.com>
2025-01-24 13:06:39 -0300

    agente Carro criado, se move pelo mapa

--- src/models.py
+++ src/models.py
@@ -1,6 +1,14 @@
 import mesa
-import numpy as np
-import pandas as pd
+import random
+
+class TrafficCell(mesa.Agent):
+    def __init__(self, model, cell_type, allowed_turn=None):
+        super().__init__(model)
+        self.cell_type = cell_type
+        self.allowed_turn = allowed_turn
+    
+    def step(self):
+        pass
 
 class TrafficLightAgent(mesa.Agent):
     def __init__(self, model, state="red"):
@@ -8,12 +16,54 @@ class TrafficLightAgent(mesa.Agent):
         self.state = state
         self.timer = 0
 
-    def switch_state(self):
+    def step(self):
         self.timer += 1
-        print(f"Eu sou o agente: {str(self.unique_id)}, estou {self.state}, e meu timer é {self.timer}.")
+        #print(f"Eu sou o agente: {str(self.unique_id)}, estou {self.state}, e meu timer é {self.timer}.")
         if self.timer > 10:
             self.state = "green" if self.state == "red" else "red"
             self.timer = 0
+
+class CarAgent(mesa.Agent):
+    def __init__(self, model, direction):
+        super().__init__(model)
+        self.direction = direction # cardinal directions (n,w,s,e)
+        self.path = []
+
+    def go_straight(self):
+        print(f"Car {self.unique_id} with direction {self.direction} is going straight at {self.pos}")
+        if self.direction == "n":
+            return (self.pos[0], self.pos[1]+1)
+        
+        if self.direction == "s":
+            return (self.pos[0], self.pos[1]-1)
+        
+        if self.direction == "w":
+            return (self.pos[0]-1, self.pos[1])
+        
+        if self.direction == "e":
+            return (self.pos[0]+1, self.pos[1])
+    
+    def turn(self):
+        print(f"Car {self.unique_id} is turning at {self.pos}")
+        if self.direction == "n":
+            self.direction = "e"
+        elif self.direction == "s":
+            self.direction = "w"
+        elif self.direction == "w":
+            self.direction = "n"
+        elif self.direction == "e":
+            self.direction = "s"
+    
+    def step(self):
+        current_cell_contents = self.model.grid.get_cell_list_contents([self.pos])
+        for agent in current_cell_contents:
+            if isinstance(agent, TrafficCell) and agent.cell_type == "intersection":
+                if agent.allowed_turn == self.direction:
+                    action = random.choice([self.turn, None])
+                    if action:
+                        action()
+        self.model.grid.move_agent(self, self.go_straight())
+        
 
 class TrafficModel(mesa.Model):
     def __init__(self, n, width, height, seed=None):
@@ -23,13 +73,45 @@ class TrafficModel(mesa.Model):
         self.datacollector = mesa.DataCollector(
             model_reporters={"Lights": "n"}, agent_reporters={"State": "state"}
         )
+
+        for x in range(self.grid.width):
+            for y in range(self.grid.height): # buildings
+                if (x<=3 and y<=3 or 
+                    y==5 and x<=3 or
+                    y>=7 and x<=3 or
+                    y==5 and x>=7 or
+                    y>=7 and x>=7 or
+                    y<=3 and x>=7 or
+                    x==5 and y<=3 or
+                    x==5 and y>=7 or
+                    x==5 and y==5):
+                    cell = TrafficCell(model=self, cell_type="building")
+                    self.grid.place_agent(cell, (x, y))
+                    
+                # Intersections
+                if (x==6 and y==4):
+                    cell = TrafficCell(model=self, cell_type="intersection", allowed_turn="n")
+                    self.grid.place_agent(cell, (x, y))
+                if (x==4 and y==4):
+                    cell = TrafficCell(model=self, cell_type="intersection", allowed_turn="e")
+                    self.grid.place_agent(cell, (x, y))
+                if (x==6 and y==6):
+                    cell = TrafficCell(model=self, cell_type="intersection", allowed_turn="w")
+                    self.grid.place_agent(cell, (x, y))
+                if (x==4 and y==6):
+                    cell = TrafficCell(model=self, cell_type="intersection", allowed_turn="s")
+                    self.grid.place_agent(cell, (x, y))
+        
+        car = CarAgent(self, direction="n")
+        self.grid.place_agent(car, (6,0))
+
         agents = TrafficLightAgent.create_agents(model=self, n=n)
-        x = self.rng.integers(0, self.grid.width, size=(n,))
-        y = self.rng.integers(0, self.grid.height, size=(n,))
-        for a, i, j in zip(agents, x, y):
-            # Add the agent to a random grid cell
-            self.grid.place_agent(a, (i, j))
+        self.grid.place_agent(agents[0], (3, 4))
+        self.grid.place_agent(agents[1], (4, 7))
+        self.grid.place_agent(agents[2], (6, 3))
+        self.grid.place_agent(agents[3], (7, 6))
+
     
     def step(self):
         self.datacollector.collect(self)
-        self.agents.do("switch_state")
+        self.agents.do("step")
--- src/server.py
+++ src/server.py
@@ -1,18 +1,26 @@
 import mesa
-from mesa.visualization import SolaraViz, make_plot_component, make_space_component
-from models import TrafficModel, TrafficLightAgent
+from mesa.visualization import SolaraViz, make_space_component
+from models import *
 
 def agent_portrayal(agent):
-    color = "tab:red" if agent.state == "red" else "tab:green"
-    return {"color": color, "size": 50}
+    if isinstance(agent, TrafficCell):
+        if agent.cell_type == "building":
+            return {"color": "tab:gray", "marker": "s", "zorder": 0}
+        if agent.cell_type == "intersection":
+            return {"color": "white", "marker": "s", "zorder": -1} 
+    if isinstance(agent, TrafficLightAgent):
+        color = "tab:red" if agent.state == "red" else "tab:green"
+        return {"color": color, "size": 50, "zorder": 1}
+    if isinstance(agent, CarAgent):
+        return {"color": "tab:blue", "zorder": 0}
 
 model_params = {
-    "n": 10,
+    "n": 4,
     "width": 10,
     "height": 10,
 }
 
-tr_model = TrafficModel(n=10, width=10, height=10)
+tr_model = TrafficModel(n=4, width=11, height=11)
 
 SpaceGraph = make_space_component(agent_portrayal)
 
